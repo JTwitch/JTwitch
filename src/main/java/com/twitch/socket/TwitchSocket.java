e98cd5a4e093ae27081699bed89d0c0b229ad8c3
CharlesLgn <bvxyz@hotmail.fr>
2021-07-27 13:24:26 +0200

    refactorinng socket

--- src/main/java/com/twitch/socket/TwitchSocket.java
+++ src/main/java/com/twitch/socket/TwitchSocket.java
@@ -9,6 +9,9 @@ import org.java_websocket.handshake.ServerHandshake;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 class TwitchSocket extends WebSocketClient {
 
@@ -27,7 +30,7 @@ class TwitchSocket extends WebSocketClient {
   public void onOpen(ServerHandshake serverHandshake) {
     System.out.println("Websocket, Open ");
     send("PASS oauth:" + connectionOption.getToken());
-    send("NICK "       + connectionOption.getUser());
+    send("NICK " + connectionOption.getUser());
     send("CAP REQ :twitch.tv/membership");
     send("CAP REQ :twitch.tv/tags");
     send("CAP REQ :twitch.tv/commands");
@@ -35,12 +38,7 @@ class TwitchSocket extends WebSocketClient {
 
   @Override
   public void onMessage(String message) {
-    if (message.startsWith("PING")) {
-      send("PONG");
-    } else if (message.contains(" PRIVMSG ")) {
-      messageActions.forEach(messageAction -> messageAction.execute(bot, new Message(message)));
-    } else if (message.contains(" WHISPER ")) {
-    }
+    SocketMessage.process(this, message);
     System.err.println(message);
   }
 
@@ -52,5 +50,30 @@ class TwitchSocket extends WebSocketClient {
   @Override
   public void onError(Exception e) {
     System.out.println("Websocket, Error " + e.getMessage());
+  }
+
+  private void executeMessageActions(String message) {
+    messageActions.forEach(messageAction -> messageAction.execute(bot, new Message(message)));
+  }
+
+  private enum SocketMessage {
+    PING   (message -> message.startsWith("PING"),    (socket, message) -> socket.send("PONG")),
+    PRIVMSG(message -> message.contains(" PRIVMSG "), TwitchSocket::executeMessageActions),
+    WHISPER(message -> message.contains(" WHISPER "), (socket, message) -> {}),
+    ;
+
+    private final Predicate<String> socketMessageType;
+    private final BiConsumer<TwitchSocket, String> actions;
+
+    SocketMessage(Predicate<String> socketMessageType, BiConsumer<TwitchSocket, String> actions) {
+      this.socketMessageType = socketMessageType;
+      this.actions = actions;
+    }
+
+    static void process(TwitchSocket socket, String message) {
+      Stream.of(values())
+            .filter(socketMessage -> socketMessage.socketMessageType.test(message))
+            .forEach(socketMessage -> socketMessage.actions.accept(socket, message));
+    }
   }
 }
